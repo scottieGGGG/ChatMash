dd55ea23ab3e57a865ba8869219743d455e072c0
unknown <ScottGAdmin@T42010118-11.ad.austinpowder.com>
2016-01-05 16:00:14 -0500

    v2

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
  io.emit('chat message', msg);
---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+var socket = io();
+$('form').submit(function () {
+    socket.emit('chat message', $('#message-box').val());
+    $('#message-box').val('');
+    return false;
+});
+socket.on('chat message', function (msg) {
+    $('#messages').append($('<p>').text(msg));
+});
